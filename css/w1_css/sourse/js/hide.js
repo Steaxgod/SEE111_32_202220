09234b5b17174f37273763d29187300f56a49fb1
Hasanov <fhasanov@email.neit.edu>
2022-04-06 09:33:54 -0400

    css_w1_end

--- css/w1_css/sourse/js/hide.js
+++ css/w1_css/sourse/js/hide.js
@@ -1,42 +1,61 @@
 // Links
-var bio = document.querySelector(`#bio`)
-var edu = document.querySelector(`#edu`)
-var hob = document.querySelector(`#hobbies`)
+var bio = document.querySelector(`#bio-link`)
+var edu = document.querySelector(`#edu-link`)
+var hob = document.querySelector(`#hob-link`)
+var all = document.querySelector(`#all-link`)
 
 // Divs
 var main = document.querySelector(`#main `)
 var education = document.querySelector(`#education`)
 var hobbies = document.querySelector(`#hobbies`)
 
-bio.addEventListener(`click`,showBio )
+// Functions
+
+bio.addEventListener(`click`, showBio)
 function showBio(e)
 {
     //keeps link from navigating to another URL
     e.preventDefault()
-    edu.classList.remove(`hide`)
-    hob.classList.add(`hide`)
+    main.classList.remove(`hide`)
+    education.classList.add(`hide`)
+    hobbies.classList.add(`hide`)
    
 
 }
 
-edu.addEventListener(`click`,showEdu )
+edu.addEventListener(`click`, showEdu)
 function showEdu(e)
 {
     //keeps link from navigating to another URL
     e.preventDefault()
-    bio.classList.remove(`hide`)
-    hob.classList.add(`hide`)
+    
+    education.classList.remove(`hide`)
+    main.classList.add(`hide`)
+    hobbies.classList.add(`hide`)
    
 
 }
 
-hob.addEventListener(`click`,showHob )
+hob.addEventListener(`click`, showHob)
 function showHob(e)
 {
     //keeps link from navigating to another URL
     e.preventDefault()
-    edu.classList.remove(`hide`)
-    bio.classList.add(`hide`)
+    hobbies.classList.remove(`hide`)
+    education.classList.add(`hide`)
+    main.classList.add(`hide`)
+   
+
+}
+
+all.addEventListener(`click`, showAll)
+function showAll(e)
+{
+    //keeps link from navigating to another URL
+    e.preventDefault()
+    hobbies.classList.remove(`hide`)
+    education.classList.remove(`hide`)
+    main.classList.remove(`hide`)
    
 
 }
