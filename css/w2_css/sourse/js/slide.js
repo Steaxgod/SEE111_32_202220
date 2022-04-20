d77ea8538a09288a45a0c4972ac68992b171a5f5
Steaxgod <fhasanov@email.neit.edu>
2022-04-20 02:23:34 -0400

    m

--- css/w2_css/sourse/js/slide.js
+++ css/w2_css/sourse/js/slide.js
@@ -6,58 +6,56 @@ var alllinks = document.querySelector(`#all-link`)
 
 
 // Creating the variables.
-
-var bio = document.querySelector(`bio`)
-var edu = document.querySelector(`edu`)
-var hob = document.querySelector(`hob`)
+var bio = document.querySelector(`#bio`)
+var edu = document.querySelector(`#edu`)
+var hob = document.querySelector(`#hob`)
 
 // Adding an a Event Listener
-
 biolinks.addEventListener(`click`, showBio)
 function showBio(e)
 {
+
     //keeps link from navigating to another URL
     e.preventDefault()
     bio.classList.remove(`hide`)
     edu.classList.add(`hide`)
     hob.classList.add(`hide`)
-   
 
 }
 
 edulinks.addEventListener(`click`, showEdu)
 function showEdu(e)
 {
+
     //keeps link from navigating to another URL
     e.preventDefault()
     
     edu.classList.remove(`hide`)
     bio.classList.add(`hide`)
     hob.classList.add(`hide`)
-   
 
 }
 
 hoblinks.addEventListener(`click`, showHob)
 function showHob(e)
 {
+
     //keeps link from navigating to another URL
     e.preventDefault()
     hob.classList.remove(`hide`)
     edu.classList.add(`hide`)
     bio.classList.add(`hide`)
-   
 
 }
 
 alllinks.addEventListener(`click`, showAll)
 function showAll(e)
 {
+
     //keeps link from navigating to another URL
     e.preventDefault()
     hob.classList.remove(`hide`)
     edu.classList.remove(`hide`)
     bio.classList.remove(`hide`)
    
-
 }
